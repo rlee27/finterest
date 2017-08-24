6be3e15694d97b130354e7e5e92a1c95d92f4fdb
Robert <rklee27@gmail.com>
2017-08-23 21:18:29 -0400

    Add model validations and seeds

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id              :integer          not null, primary key
+#  title           :string           not null
+#  author_id       :integer          not null
+#  collaborator_id :integer
+#  topic_id        :integer
+#  cover_id        :integer
+#  description     :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Board < ApplicationRecord
+  validates :title, :author_id, presence: true
+
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+user1 = User.create!(email: "password", password: "password", name: "Main Test Dummy")
+user2 = User.create!(email: "example@example.com", password: "password", name: "Example Doe")
+user3 = User.create!(email: "john@example.com", password: "password", name: "John Smith")
+user4 = User.create!(email: "jane@example.com", password: "password", name: "Jane Doe")
+
+Board.destroy_all
+board1 = Board.create!(title: "ExampleBoard1", author_id: user1.id)
+board2 = Board.create!(title: "ExampleBoard1", author_id: user2.id)
+board3 = Board.create!(title: "ExampleBoard1", author_id: user3.id)
