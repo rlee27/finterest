2859d3d6f97838bd4301588fdfadae424a925471
Robert <rklee27@gmail.com>
2017-08-25 15:07:20 -0400

    Add users to entities slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import boards from './board_reducer';
+import users from './user_reducer';
 
 export default combineReducers({
   boards,
+  users,
 });
---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,24 @@
+import { RECEIVE_USER, RECEIVE_ERRORS } from '../actions/user_actions';
+import { merge } from 'lodash';
+
+const defaultState = {
+  user: {},
+  errors: [],
+};
+
+const UserReducer = (state = defaultState, action) => {
+  let nextState;
+
+  switch (action.type) {
+    case RECEIVE_USER:
+      nextState = merge({}, state, {user: action.user});
+      return nextState;
+    case RECEIVE_ERRORS:
+      nextState = merge({}, state, {errors: action.errors});
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
