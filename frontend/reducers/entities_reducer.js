b3ea594d6e25d069a122b137a3088c7115cbc498
Robert <rklee27@gmail.com>
2017-08-27 23:20:20 -0400

    Curate slice of state to include pins

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import boards from './board_reducer';
 import users from './user_reducer';
+import pins from './pin_reducer';
 
 export default combineReducers({
   boards,
   users,
+  pins,
 });
