8273dfd3557455b161cd9d8bbef84db2c69d9c70
Robert <rklee27@gmail.com>
2017-08-30 14:59:11 -0400

    Change backend to adjust pin_utl

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,21 +2,34 @@
 #
 # Table name: pins
 #
-#  id          :integer          not null, primary key
-#  pin_url     :string           not null
-#  author_id   :integer          not null
-#  board_id    :integer          not null
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  topic_id    :integer
+#  id                 :integer          not null, primary key
+#  pin_url            :string
+#  author_id          :integer          not null
+#  board_id           :integer          not null
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  topic_id           :integer
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
+
+
 
 class Pin < ApplicationRecord
   validates :pin_url, :author, :board, presence: true
 
   has_attached_file :image, default_url: "pin-default.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  after_save :ensure_pin_url
+
+
+  def enusure_pin_url
+    self.pin_url ||= self.image.url
+  end
 
   belongs_to :board,
     class_name: :Board,
