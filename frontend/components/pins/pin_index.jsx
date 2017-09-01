80a0c00ed460bd4d7fa1225b8c53dc6c8c79d51c
Robert <rklee27@gmail.com>
2017-08-31 20:30:40 -0400

    Link to pin show

--- frontend/components/pins/pin_index.jsx
+++ frontend/components/pins/pin_index.jsx
@@ -3,25 +3,28 @@ import { values } from 'lodash';
 import NewPinContainer from './new_pin_container';
 import EditPinContainer from './edit_pin_container';
 import SavePinContainer from './save_pin_container';
+import { Link } from 'react-router-dom';
 
 class PinIndex extends React.Component {
   pinList() {
     const allUserPins = values(this.props.userPins);
     return allUserPins.map((pin) => {
       return (
-        <li key={pin.id} className="pin-card">
-          <div className="pin-options">
-            <div className="pin-edit-button">
-              <EditPinContainer pin={pin} />
+        <Link to={`/pins/${pin.id}`} key={pin.id}>
+          <li className="pin-card">
+            <div className="pin-options">
+              <div className="pin-edit-button">
+                <EditPinContainer pin={pin} />
+              </div>
+              <div className="save-button">
+                <SavePinContainer pin={pin} />
+              </div>
             </div>
-            <div className="save-button">
-              <SavePinContainer pin={pin} />
+            <div className="pin-cover">
+              <img src={pin.image_url} className="pin-image"/>
             </div>
-          </div>
-          <div className="pin-cover">
-            <img src={pin.image_url} className="pin-image"/>
-          </div>
-        </li>
+          </li>
+        </Link>
       );
     });
   }
