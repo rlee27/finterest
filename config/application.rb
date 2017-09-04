57530ad2450917de461c5fde39eec6e11527d7f3
Robert <rklee27@gmail.com>
2017-09-03 22:37:21 -0400

    Try to fix mixed content issue on production

--- config/application.rb
+++ config/application.rb
@@ -12,29 +12,29 @@ module PinterestClone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_region => ENV["AWS_REGION"],
-    :s3_credentials => {
-      :bucket => ENV["s3_bucket"],
-      :access_key_id => ENV["s3_access_key_id"],
-      :secret_access_key => ENV["s3_secret_access_key"],
-      :s3_region => ENV["s3_region"],
-      :s3_host_name => "s3.amazonaws.com",
-      :url => ":s3_host_name"
-    }
-  }
-  end
   # config.paperclip_defaults = {
   #   :storage => :s3,
+  #   :s3_region => ENV["AWS_REGION"],
   #   :s3_credentials => {
   #     :bucket => ENV["s3_bucket"],
   #     :access_key_id => ENV["s3_access_key_id"],
   #     :secret_access_key => ENV["s3_secret_access_key"],
   #     :s3_region => ENV["s3_region"],
-  #     :s3_host_name => "s3-#{ENV['s3_region']}.amazonaws.com",
-  #     :url => :s3_host_name
+  #     :s3_host_name => "https://s3.amazonaws.com",
+  #     :url => ":s3_host_name"
   #   }
   # }
   # end
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV["s3_bucket"],
+      :access_key_id => ENV["s3_access_key_id"],
+      :secret_access_key => ENV["s3_secret_access_key"],
+      :s3_region => ENV["s3_region"],
+      :s3_host_name => "s3-#{ENV['s3_region']}.amazonaws.com",
+      :url => :s3_host_name
+    }
+  }
+  end
 end
