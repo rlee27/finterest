24b8a32016bd730d8315d7638aa435489f6aa5eb
Robert <rklee27@gmail.com>
2017-09-26 18:31:30 -0400

    Incoporate dev environment logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,11 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const {createLogger} = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   );
 };
 
